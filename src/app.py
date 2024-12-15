b8464dacb602283fb14a5fd75d5f726b90d9a456
rsptur <rturtio@gmail.com>
2024-12-15 08:36:23 +0200

    fix UI

--- src/app.py
+++ src/app.py
@@ -8,13 +8,14 @@ Ohjelman käyttöliittymä
 """
 if __name__ == '__main__':
     print("Valitse alla olevista vaihtoehtoista")
+    print("Jos sinulla ei ole salausavaimia, aloita valitsemalla 1")
     print("1. Tee avaimet")
     print("2. Salaa viesti")
     print("3. Pura viesti")
     print("4. Lopeta")
     jatka = True
     while jatka == True:
-        valinta = input("Syötä numero:")
+        valinta = input("Syötä numero: ")
         oikea = False
         while not oikea:
             try:
@@ -27,33 +28,35 @@ if __name__ == '__main__':
                 print("Muodostetaan avaimia...")
                 Keys.publickey()
             elif valinta == 2:
-                message1 = input("Kirjoita salattava viesti:")
+                message1 = input("Kirjoita salattava viesti: ")
                 while len(message1) < 1:
-                    print("Et antanut salattavaa viestiä:")
-                    message1 = input("Kirjoita salattava viesti:")
-                javain = int(input("Anna julkinen avain:"))
-                while len(str(javain)) < 1:
-                    print("Et antanut julkista avainta:")
-                    javain = int(input("Anna julkinen avain:"))
+                    print("Et antanut salattavaa viestiä")
+                    message1 = input("Kirjoita salattava viesti: ")
+                javain = int(input("Anna julkinen avain: "))
+                while len(str(javain)) < 1 or javain<1:
+                    print("Et antanut käypää julkista avainta")
+                    javain = int(input("Anna julkinen avain: "))
                 tulo = int(input("Anna alkulukujen tulo: "))
-                while len(str(tulo)) < 1:
-                    print("Et antanut alkulukujen tuloa:")
+                while len(str(tulo)) < 1 or tulo<1:
+                    print("Et antanut käypää alkulukujen tuloa")
                     tulo = int(input("Anna alkulukujen tulo:"))
+                print("Viestiä salataan...Tämä vie jotain minuutteja")
                 tulos1 = Encryption.encoder(message1, javain, tulo)
                 print(tulos1)
             elif valinta == 3:
-                message2 = input("Purettava viesti:")
+                message2 = input("Purettava viesti: ")
                 while len(message2) < 1:
-                    print("Et antanut purettavaa viestiä:")
-                    message1 = input("Anna purettava viesti:")
+                    print("Et antanut purettavaa viestiä")
+                    message1 = input("Anna purettava viesti: ")
                 tulo = int(input("Anna alkulukujen tulo: "))
-                while len(str(tulo)) < 1:
-                    print("Et antanut alkulukujen tuloa:")
-                    tulo = int(input("Anna alkulukujen tulo:"))
-                yavain = int(input("Anna yksityinen avain:"))
-                while len(str(javain)) < 1:
-                    print("Et antanut yksityistä avainta:")
-                    yavain = int(input("Anna yksityinen avain:"))
+                while len(str(tulo)) < 1 or tulo<1:
+                    print("Et antanut alkulukujen tuloa")
+                    tulo = int(input("Anna alkulukujen tulo: "))
+                yavain = int(input("Anna yksityinen avain: "))
+                while len(str(yavain)) < 1 or yavain<1:
+                    print("Et antanut yksityistä avainta")
+                    yavain = int(input("Anna yksityinen avain: "))
+                print("Viestiä puretaan... Tämä vie jotain minuutteja")
                 tulos2 = Decryption.decoder(message2, yavain, tulo)
                 print(tulos2)
             elif valinta == 4:
--- src/generator.py
+++ src/generator.py
@@ -9,7 +9,8 @@ class PQgeneraattori:
     """
     def generaattori():
         while True:
-            bits = 1024
+            bits = 51
+            #bits = 1024
             luku1 = Primet.pienetprimet(bits)
             if not Primet.MillerRabintesti(luku1):
                 continue
@@ -17,7 +18,8 @@ class PQgeneraattori:
                 p = luku1
                 break
         while True:
-            bits = 1024
+            bits = 51
+            #bits = 1024
             luku2 = Primet.pienetprimet(bits)
             if not Primet.MillerRabintesti(luku2):
                 continue
